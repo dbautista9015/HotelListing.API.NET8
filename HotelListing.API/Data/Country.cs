df265f88a7321c675cdbf12fd953dfb955a99dba
Daniel Bautista <dbautista9015@gmail.com>
2024-08-04 12:23:57 -0700

    Create CountriesController and make HotelList nullable

--- HotelListing.API/Data/Country.cs
+++ HotelListing.API/Data/Country.cs
@@ -6,6 +6,6 @@ namespace HotelListing.API.Data
         public string Name { get; set; }
         public string ShortName { get; set; }
 
-        public virtual IList<Hotel> Hotels { get; set; }
+        public virtual IList<Hotel>? Hotels { get; set; }
     }
 }
